fc969a568371254ed9151c44e620173bd9dcfbc0
rddeuser <bdeuser13@gmail.com>
2021-11-17 17:22:20 -0600

    Add delete method
    Sprint 1 User Story 8 add delete method for courses

--- src/main/java/dmacc/controller/WebController.java
+++ src/main/java/dmacc/controller/WebController.java
@@ -1,5 +1,6 @@
 /**
  * @author alexanderpontier - ampontier1
+ * @author becca deuser
  * CIS175 - Spring 2021
  * Nov 17, 2021
  */
@@ -7,7 +8,11 @@ package dmacc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import dmacc.beans.Course;
 import dmacc.repository.CourseRegistrationRepository;
 
 @Controller
@@ -15,5 +20,13 @@ public class WebController {
 	@Autowired
 	CourseRegistrationRepository repo;
 	
-
+	//delete function
+	@GetMapping("/delete/{id}")
+	
+	public String deleteCourse(@PathVariable("id") long id, Model model) {
+		Course c = repo.findById(id).orElse(null);
+		repo.delete(c);
+		//TODO: viewAllCourses method will need to be created
+		return viewAllCourses(model);
+	}//end deleteUser
 }
